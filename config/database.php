bdacfde744a2d0731b643ca7516d84474f253396
sagansa <asapanganbangsa@gmail.com>
2026-06-06 09:10:21 +0700

    Update database.php

--- config/database.php
+++ config/database.php
@@ -91,7 +91,7 @@ return [
             'port' => env('DB_RECRUITMENT_PORT', '3306'),
             'database' => env('DB_RECRUITMENT_DATABASE', 'sagansa_recruitment'),
             'username' => env('DB_RECRUITMENT_USERNAME', 'root'),
-            'password' => env('DB_RECRUITMENT_PASSWORD', ''),
+            'password' => env('DB_RECRUITMENT_PASSWORD') ?: env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => env('DB_CHARSET', 'utf8mb4'),
             'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
